99427d699c8bc140157fdf213b04e0b4d938e4fa
Rafael <38105703+MRxRafi@users.noreply.github.com>
2025-09-26 20:00:08 +0200

    Update cumple.ts

--- api/cumple.ts
+++ api/cumple.ts
@@ -7,7 +7,7 @@ const urlMap: Record<string, string> = {
   '3': 'https://tu_otro_destino.com',
 };
 
-const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
+const SLACK_WEBHOOK_URL = 'https://hooks.slack.com/services/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';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   // Enviar notificación Slack
